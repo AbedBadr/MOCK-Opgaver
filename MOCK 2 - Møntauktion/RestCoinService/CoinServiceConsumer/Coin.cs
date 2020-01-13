08c4b262e6a2ab719af84d6e26860ea8493507b4
AbedBadr <45338826+AbedBadr@users.noreply.github.com>
2020-01-13 02:50:45 +0100

    Add files via upload

---
+++ MOCK 2 - Møntauktion/RestCoinService/CoinServiceConsumer/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinServiceConsumer
+{
+    class Coin
+    {
+        public int Id { get; set; }
+        public string Genstand { get; set; }
+        public int Bud { get; set; }
+        public string Navn { get; set; }
+
+        public Coin(int id, string genstand, int bud, string navn)
+        {
+            Id = id;
+            Genstand = genstand;
+            Bud = bud;
+            Navn = navn;
+        }
+
+        public Coin()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Genstand)}: {Genstand}, {nameof(Bud)}: {Bud}, {nameof(Navn)}: {Navn}";
+        }
+    }
+}
---
+++ MOCK 2 - Møntauktion/RestCoinService/CoinServiceConsumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoinServiceConsumer
+{
+    class Program
+    {
+        private static string coinUri = "https://localhost:44358/api/Coins/";
+
+        static void Main(string[] args)
+        {
+            HttpRequests();
+            Console.ReadKey();
+        }
+
+        public static async void HttpRequests()
+        {
+            Console.WriteLine("GET ALL COINS REQUEST");
+            List<Coin> coinList = GetCoinsRequest().Result;
+
+            foreach (Coin coin in coinList)
+            {
+                Console.WriteLine(coin);
+            }
+
+            Console.WriteLine("\nGET ONE COIN REQUEST");
+            Coin oneCoin = GetOneCoinRequest(2).Result;
+            Console.WriteLine(oneCoin);
+        }
+
+        public static async Task<List<Coin>> GetCoinsRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(coinUri);
+                List<Coin> cList = JsonConvert.DeserializeObject<List<Coin>>(content);
+                return cList;
+            }
+        }
+
+        public static async Task<Coin> GetOneCoinRequest(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(coinUri + id);
+                Coin coin = JsonConvert.DeserializeObject<Coin>(content);
+                return coin;
+            }
+        }
+    }
+}
