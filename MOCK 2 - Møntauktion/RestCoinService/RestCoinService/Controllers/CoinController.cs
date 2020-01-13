dcf5fc2f06b183c66fb428572cd7583de8c831a1
AbedBadr <45338826+AbedBadr@users.noreply.github.com>
2020-01-13 02:54:07 +0100

    Add files via upload

---
+++ MOCK 2 - Møntauktion/RestCoinService/RestCoinService/Controllers/CoinController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCoinService.Model;
+
+namespace RestCoinService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinController : ControllerBase
+    {
+        public static List<Coin> coinList = new List<Coin>()
+        {
+            new Coin(1, "Gold DK 1640", 2500, "Mike"),
+            new Coin(2, "Gold NL 1764", 5000, "Anbo"),
+            new Coin(3, "Gold FR 1644", 35000, "Hammer"),
+            new Coin(4, "Gold FR 1644", 0, "Auction"),
+            new Coin(5, "Silver GR 333", 2500, "Mike")
+        };
+
+        [HttpGet]
+        [Route("/api/Coins")]
+        public List<Coin> GetCoins()
+        {
+            return coinList;
+        }
+
+        [HttpGet]
+        [Route("/api/Coins/{id}")]
+        public Coin GetOneCoin(int id)
+        {
+            return coinList.Find(c => c.Id == id);
+        }
+
+        [HttpPost]
+        [Route("/api/AddCoin")]
+        public void AddCoin(Coin newCoin)
+        {
+            coinList.Add(newCoin);
+        }
+
+        [HttpPut]
+        [Route("/api/EditCoin/{id}")]
+        public void EditCoin(int id, Coin editCoin)
+        {
+            Coin coin = coinList.Find(c => c.Id == id);
+            
+        }
+    }
+}
---
+++ MOCK 2 - Møntauktion/RestCoinService/RestCoinService/Model/Coin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCoinService.Model
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Genstand { get; set; }
+        public int Bud { get; set; }
+        public string Navn { get; set; }
+
+        public Coin(int id, string genstand, int bud, string navn)
+        {
+            Id = id;
+            Genstand = genstand;
+            Bud = bud;
+            Navn = navn;
+        }
+
+        public Coin()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Genstand)}: {Genstand}, {nameof(Bud)}: {Bud}, {nameof(Navn)}: {Navn}";
+        }
+    }
+}
